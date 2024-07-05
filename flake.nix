da91262c2082692d4ef1f226b906402f45842b2c
John Doe <johndoe@example.com>
2024-07-05 20:13:32 +0200

    Added nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+# flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      #       ↑ Swap it for your system if needed
+      #       "aarch64-linux" / "x86_64-darwin" / "aarch64-darwin"
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          rustc
+          rust-analyzer
+          rustfmt
+          pkg-config
+          openssl
+        ];
+
+        env = {
+          RUST_BACKTRACE = "full";
+        };
+      };
+    };
+}
